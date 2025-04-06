ee6504bfff8e74461301b2cac5cccf65ad9bf8a1
Wooyoung Jung <wooyoung3864@naver.com>
2025-04-07 02:21:53 +0900

    wip

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <button className="logo">
-        <img src="/assets/imgs/nagai_logo.png" alt="nagAI logo" className="logo-img" />
+        <img src="../../assets/imgs/nagai_logo.png" alt="nagAI logo" className="logo-img" />
       </button>
       <button className="username">WooHyoung Ji</button>
     </div>

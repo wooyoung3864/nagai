876570c1597c04571d7eb1cbb5045a99477f4f0d
Wooyoung Jung <wooyoung3864@naver.com>
2025-04-07 02:19:47 +0900

    wip

---
+++ src/components/WebcamFeed/WebcamFeed.tsx
@@ -0,0 +1,11 @@
+// components/WebcamFeed.tsx
+import React from 'react';
+import './WebcamFeed.css';
+
+export default function WebcamFeed() {
+  return (
+    <div className="webcam-feed">
+      <img src="/assets/webcam-sample.jpg" alt="Webcam feed" />
+    </div>
+  );
+}

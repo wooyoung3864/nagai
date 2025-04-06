b923be2d127199af48ea049ddecb34ab1b8105e4
Wooyoung Jung <wooyoung3864@naver.com>
2025-04-07 02:45:05 +0900

    first draft for Navbar & MainPage: wyjung

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 // components/Navbar.tsx
 import React from 'react';
+import logo from '../../assets/imgs/nagai_logo.png';
+
 import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <button className="logo">
-        <img src="../../assets/imgs/nagai_logo.png" alt="nagAI logo" className="logo-img" />
+      <img src={logo} alt="nagAI logo" className="logo-img" />
       </button>
       <button className="username">WooHyoung Ji</button>
     </div>

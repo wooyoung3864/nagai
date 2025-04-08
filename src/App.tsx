4ade1294203e78c47bfef37ff6266e2d4d4913a6
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-04-08 16:50:55 +0900

    modified loginfailpage termspage, accountcreationpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
+import LoginFailPage from './pages/LoginFailPage/LoginFailPage';
+import TermsPage from './pages/TermsPage/TermsPage';
+import AccountCreationPage from './pages/AccountCreationPage/AccountCreationPage';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="app-container">
+    /*<div className="app-container">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </div>
+    */
+   <AccountCreationPage />
   );
+  
 }
--- src/pages/AccountCreationPage/AccountCreationPage.tsx
+++ src/pages/AccountCreationPage/AccountCreationPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AccountCreationPage.css';
 
 export default function AccountCreationPage() {
   return (

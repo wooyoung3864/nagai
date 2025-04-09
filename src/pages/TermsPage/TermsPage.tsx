2a9b24e47e2adeae5109ea21469998174a89d37c
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-04-09 13:16:13 +0900

    added checkbox function in termspage: joo

--- src/pages/TermsPage/TermsPage.tsx
+++ src/pages/TermsPage/TermsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TermsPage.css'
 
@@ -6,7 +6,14 @@ import './TermsPage.css'
 export default function TermsPage() {
   const navigate = useNavigate();
 
+  const [agreeTerms, setAgreeTerms] = useState(false);
+  const [agreePrivacy, setAgreePrivacy] = useState(false);
+
   const handleContinueClick = () => {
+    if(!agreePrivacy || !agreeTerms){
+      window.alert("Terms and Conditions Agreement Required!");
+      return;
+    }
     // optional: auth logic here
     navigate('/account-creation');
   };
@@ -19,25 +26,34 @@ export default function TermsPage() {
       <div className="terms-logo">
         <img src="src/assets/imgs/nagai_logo.png" alt="nagai_logo" />
       </div>
+      <p />
       <h2>Terms and Conditions</h2>
-
       <div className="terms-scroll-box">
         By creating an account, you agree to our Terms and Conditions. <br /><br />
         You consent to the use of your webcam for the purpose of real-time interaction and learning enhancement. <br />
         You also agree to allow the application to access and use your Google account information, such as your name and email, solely for identification purposes.
       </div>
       <label className="terms-checkbox-label">
-        <input type="checkbox" />
+        <input 
+          type="checkbox"
+          checked={agreeTerms}
+          onChange={(e) => setAgreeTerms(e.target.checked)}
+        />
         I have read and agree to the Terms and Conditions.
       </label>
-
+      <h2>Privacy Agreement</h2>
+      <p />
       <div className="terms-scroll-box">
         By continuing, you agree to the collection and use of your personal information, including your name, email, and activity data, as outlined in our Privacy Policy. <br /><br />
         Your information will be used solely for improving service quality and will not be shared with third parties without your consent.<br /><br />
         Also, the recorded video data of you will be stored for seven days on the server and removed after seven days it is recorded. 
         </div>
       <label className="terms-checkbox-label">
-        <input type="checkbox" />
+      <input 
+          type="checkbox"
+          checked={agreePrivacy}
+          onChange={(e) => setAgreePrivacy(e.target.checked)}
+        />
         I have read and agree to the Privacy Statement.
       </label>
 

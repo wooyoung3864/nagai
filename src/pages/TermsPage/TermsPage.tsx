6fe0cd41822a4005c2943260baac9bc2abe593c4
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-04-13 21:34:10 +0900

    dropdown in terms page modified

--- src/pages/TermsPage/TermsPage.tsx
+++ src/pages/TermsPage/TermsPage.tsx
@@ -58,19 +58,26 @@ export default function TermsPage() {
 
       {/* Terms Section */}
       <div className="terms-accordion-card">
-        <div className="terms-accordion-header" onClick={() => setOpenTerms(!openTerms)}>
+        <motion.div 
+          className="terms-accordion-header" 
+          onClick={() => setOpenTerms(!openTerms)}
+        >
           <span>Terms and Conditions</span>
           <span>{openTerms ? '▲' : '▼'}</span>
-        </div>
+        </motion.div>
 
         {openTerms && (
-          <>
+          <motion.div
+            className="terms-scroll-box-wrapper"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+            transition={{ duration: 0.3 }}
+          >
             <div className="terms-scroll-box">
               By creating an account, you agree to our Terms and Conditions. <br /><br />
               You consent to the use of your webcam for the purpose of real-time interaction and learning enhancement. <br />
               You also agree to allow the application to access and use your Google account information, such as your name and email, solely for identification purposes.
             </div>
-
             <label className="terms-checkbox-label">
               <input
                 type="checkbox"
@@ -79,7 +86,7 @@ export default function TermsPage() {
               />
               I have read and agree to the Terms and Conditions.
             </label>
-          </>
+          </motion.div>
         )}
       </div>
 
@@ -91,13 +98,17 @@ export default function TermsPage() {
         </div>
 
         {openPrivacy && (
-          <>
+          <motion.div
+            className="terms-scroll-box-wrapper"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+            transition={{ duration: 0.3 }}
+          >
             <div className="terms-scroll-box">
               By continuing, you agree to the collection and use of your personal information, including your name, email, and activity data, as outlined in our Privacy Policy. <br /><br />
               Your information will be used solely for improving service quality and will not be shared with third parties without your consent. <br /><br />
               Also, the recorded video data of you will be stored for seven days on the server and removed after seven days it is recorded.
             </div>
-
             <label className="terms-checkbox-label">
               <input
                 type="checkbox"
@@ -106,7 +117,7 @@ export default function TermsPage() {
               />
               I have read and agree to the Privacy Statement.
             </label>
-          </>
+          </motion.div>
         )}
       </div>
 

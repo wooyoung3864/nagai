85bb064f4dbff9a2b91fcb237e9ab6db1b6271aa
Wooyoung Jung <wooyoung3864@naver.com>
2025-04-13 22:07:33 +0900

    404 page & minor UI edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import WebcamFeed from './components/WebcamFeed/WebcamFeed';
-import Timer from './components/Timer/Timer';
-import GestureHelpButton from './components/GestureHelpButton/GestureHelpButton';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
@@ -10,6 +5,7 @@ import LoginFailPage from './pages/LoginFailPage/LoginFailPage';
 import TermsPage from './pages/TermsPage/TermsPage';
 import AccountCreationPage from './pages/AccountCreationPage/AccountCreationPage';
 import MyPage from './pages/MyPage/MyPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import './App.css';
 
 export default function App() {
@@ -22,6 +18,7 @@ export default function App() {
         <Route path="/login-fail" element={<LoginFailPage />} />
         <Route path="/account-creation" element={<AccountCreationPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* 404 fallback */}
       </Routes>
     </div>
     

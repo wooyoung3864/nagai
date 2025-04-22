2e612906171a9d6028d7030f2c58e15f8ee684b8
SUNY Korea <sunyk@SUNYs-iMac.local>
2025-04-22 14:58:52 +0900

    HelpMessage draft complete

--- frontend/src/components/WebcamFeed/WebcamFeed.tsx
+++ frontend/src/components/WebcamFeed/WebcamFeed.tsx
@@ -2,6 +2,9 @@
 import React, { useEffect, useRef } from 'react';
 import './WebcamFeed.css';
 import { motion } from 'framer-motion';
+import useIsMobile from '../../hooks/useIsMobile';
+import palmImg from '../../assets/imgs/palm.png';
+import backOfHandImg from '../../assets/imgs/back_of_hand.png';
 
 interface WebcamFeedProps {
   showOverlay: boolean;
@@ -19,6 +22,7 @@ export default function WebcamFeed({
   errorMessage,
 }: WebcamFeedProps) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     const initCamera = () => {
@@ -70,7 +74,31 @@ export default function WebcamFeed({
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.3 }}
-            />
+            >
+              <div className='overlay-grid-wrap'>
+                <div className='overlay-grid-col'>
+                  <div className='overlay-grid-header'>
+                    <h2 className='overlay-text margin-left-1rem'>
+                      Raise your hand like:
+                    </h2>
+                  </div>
+                  <div className='overlay-grid-row'>
+                    <div className='overlay-grid-cell'>
+                      <img src={palmImg} className='palm-icon' alt="Palm gesture" />Ï<h2 className='overlay-text margin-left-2rem'>
+                        {isMobile ? 'to stop.' : 'to stop the timer/break.'}
+                      </h2>
+                    </div>
+                    <div className='overlay-grid-cell'>
+                      <img src={backOfHandImg} className='back-of-hand-icon' alt="Back of hand gesture" />
+                      <h2 className='overlay-text margin-left-2rem'>
+                        {isMobile ? 'to pause.' : 'to pause the timer.'}
+                      </h2>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
           )}
           <video ref={videoRef} autoPlay muted playsInline />
         </>

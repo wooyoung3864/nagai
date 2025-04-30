2170eced649e376f1c9bf46e457dad1bf737427f
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-04-30 22:08:33 +0900

    fastAPI modified

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://username:password@localhost/dbname"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+

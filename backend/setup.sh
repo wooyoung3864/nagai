3bb669e12e279cdc0e94de22a2c01478b0480e98
SUNY Korea <sunyk@SUNYs-iMac.local>
2025-05-13 17:32:56 +0900

    change python to python3

--- backend/setup.sh
+++ backend/setup.sh
@@ -3,10 +3,10 @@ conda create -n nagai python=3.10 -y          # or 3.11/3.12 if you prefer
 conda activate nagai
 
 # ensure pip is available in this env
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # install packages (some aren't on Conda yet, so pip is easier)
-pip install fastapi "uvicorn[standard]" \
+pip3 install fastapi "uvicorn[standard]" \
     sqlalchemy psycopg2-binary python-dotenv \
     supabase google-auth cryptography httpie python-multipart email-validator \
     alembic

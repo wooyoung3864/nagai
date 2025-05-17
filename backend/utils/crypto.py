3d3246896a0a719c5ff1a7038b0fd35ec6d813b0
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-05-17 18:14:29 +0900

    secretkey

--- backend/utils/crypto.py
+++ backend/utils/crypto.py
@@ -5,5 +5,5 @@ dotenv.load_dotenv()
 KEY = os.getenv("FERNET_MASTER_KEY") or Fernet.generate_key()
 cipher = Fernet(KEY)
 
-encrypt = lambda s: cipher.encrypt(s.econde()).decode()
+encrypt = lambda s: cipher.encrypt(s.encode()).decode()
 decrypt = lambda t: cipher.decrypt(t.encode()).decode()

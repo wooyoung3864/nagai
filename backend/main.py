5e04ab7193e541ee748aef9b59b8a55b5ab820df
Wooyoung Jung <wooyoung3864@naver.com>
2025-05-19 19:04:23 +0900

    backend update 2

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
                     "http://127.0.0.1:5173",
-                    "http://https://nagai-ewle8xhhc-wooyoung3864s-projects.vercel.app//", 
-                    "https://https://nagai-ewle8xhhc-wooyoung3864s-projects.vercel.app//", 
-                    "http://https://nagai-pi.vercel.app/",
-                    "https://https://nagai-pi.vercel.app/"
+                    "http://nagai-ewle8xhhc-wooyoung3864s-projects.vercel.app//", 
+                    "https://nagai-ewle8xhhc-wooyoung3864s-projects.vercel.app//", 
+                    "http://nagai-pi.vercel.app/",
+                    "https:///nagai-pi.vercel.app/"
                     ],  
     allow_credentials=True,
     allow_methods=["*"],
